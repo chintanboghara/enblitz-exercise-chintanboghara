eead9f40e5e4efe033d87a29b3981b85745f6752
Chintan Boghara <140156149+chintanboghara@users.noreply.github.com>
2024-03-28 06:12:53 +0000

    Tfvars

---
+++ EC2-Terraform-Tfvars/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  associate_public_ip_address = true
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "EC2-key-pair"
+    tags = {
+    "Name" = "EC2-Variable"
+  }
+
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.example.private_ip
+}
